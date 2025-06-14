8c65ba962427a97a5c695b3375a2c4be3a604b27
BibashDahal1 <bibash98425@gmail.com>
2025-06-14 10:04:22 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <div className='text-blue-600/100'> Hello world</div>
+
+    </>
+  )
+}
+
+export default App
