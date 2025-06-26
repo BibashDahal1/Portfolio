7d890d68e68469f775d139e53eeebf6d58d4d013
BibashDahal1 <bibash98425@gmail.com>
2025-06-26 13:08:07 +0545

    Initial commit

--- src/Services.jsx
+++ src/Services.jsx
@@ -67,7 +67,17 @@ const Service = () =>{
 
     return(
         <>
+        <div className="sticky top-0 z-50">
         <Navbar />
+      </div>
+      
+      <div className='bg-gray-100'>
+        <motion.div 
+          style={{ scaleX: scrollYProgress }}
+          className='bg-red-500 origin-left w-full h-2 md:h-3 fixed top-16 left-0 rounded-full z-40'
+        />
+      </div>
+
 
 
 
